00c2d81cecc99ab119462b8fe58ed1d28925575f
milagro2 <morenoseijs06@gmail.com>
2024-03-21 11:39:45 +0100

    styling

--- www/dashboard.php
+++ www/dashboard.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 
-// Controleer of de gebruiker is ingelogd
+
 if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
-    // Als de gebruiker niet is ingelogd, stuur hem naar de inlogpagina
     header("Location: login.php");
     exit();
 }
 
-// Gebruiker is ingelogd, toon het dashboard
 ?>
 
 <!DOCTYPE html>
@@ -22,10 +20,9 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
 <body>
     <div class="container">
         <h2>Welkom bij het Dashboard</h2>
-        <!-- Inhoud van het dashboard -->
         <p>Dit is het dashboard van de applicatie.</p>
         <p>Voeg hier inhoud toe die alleen zichtbaar is voor ingelogde gebruikers.</p>
-        <a href="login.php">Uitloggen</a> <!-- Optie om uit te loggen -->
+        <a href="index.php">Uitloggen</a>
     </div>
 </body>
 </html>
