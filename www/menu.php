3f1a48da84bb9db2e06cf7d8e7278048fcc0d710
milagro2 <morenoseijs06@gmail.com>
2024-03-25 13:49:29 +0100

    good

--- www/menu.php
+++ www/menu.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
     <link rel="stylesheet" href="styles.css">
+    <style>
+
+
+    </style>
 </head>
 
 <body>
@@ -14,20 +18,18 @@
     <?php include 'navbar.php'; ?>
 
     <div class="container">
-        <h1>Menu</h1>
+
         <?php
         // Database connection
         require_once "database.php";
 
-        // Query to select all recipes
         $sql = "SELECT * FROM Product";
         $result = mysqli_query($conn, $sql);
 
-        // Check if there are any recipes
         if (mysqli_num_rows($result) > 0) {
-            // Output data of each recipe
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '<div class="recipe">';
+                echo '<div class="recipe-details">';
                 echo '<h2>' . $row["naam"] . '</h2>';
                 echo '<p><strong>Beschrijving:</strong> ' . $row["beschrijving"] . '</p>';
                 echo '<p><strong>Inkoopprijs:</strong> €' . $row["inkoopprijs"] . '</p>';
@@ -35,6 +37,8 @@
                 echo '<p><strong>Vegan:</strong> ' . ($row["is_vega"] ? "Ja" : "Nee") . '</p>';
                 echo '<p><strong>Categorie:</strong> ' . $row["categorie"] . '</p>';
                 echo '<p><strong>Aantal voorraad:</strong> ' . $row["aantal_voorraad"] . '</p>';
+                echo '</div>';
+                echo '<img src="fotos/' . $row["afbeelding"] . '" alt="' . $row["naam"] . '">';
                 echo '</div>';
             }
         } else {
@@ -45,6 +49,7 @@
         mysqli_close($conn);
         ?>
     </div>
+
 </body>
 
 </html>
