151c6d84a2ca80c6338e93f0364f17abd91291fe
milagro2 <morenoseijs06@gmail.com>
2024-03-26 13:55:28 +0100

    goodday:)

--- www/menu.php
+++ www/menu.php
@@ -17,14 +17,26 @@
     <?php $title = "Menu"; ?>
     <?php include 'navbar.php'; ?>
 
-    
     <div class="container">
 
+        <form action="menu.php" method="get">
+            <input type="text" name="search" placeholder="Zoek gerechten...">
+            <button type="submit">Zoeken</button>
+        </form>
+
         <?php
-        // Database connection
         require_once "database.php";
 
         $sql = "SELECT * FROM Product";
+
+        if (isset($_GET['search'])) {
+            $search = mysqli_real_escape_string($conn, $_GET['search']);
+
+            if (!empty($search)) {
+                $sql = "SELECT * FROM Product WHERE naam LIKE '%$search%'";
+            }
+        }
+
         $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) > 0) {
@@ -33,26 +45,27 @@
                 echo '<div class="recipe-details">';
                 echo '<h2>' . $row["naam"] . '</h2>';
                 echo '<p><strong>Beschrijving:</strong> ' . $row["beschrijving"] . '</p>';
-                echo '<p><strong>Inkoopprijs:</strong> €' . $row["inkoopprijs"] . '</p>';
                 echo '<p><strong>Verkoopprijs:</strong> €' . $row["verkoopprijs"] . '</p>';
                 echo '<p><strong>Vegan:</strong> ' . ($row["is_vega"] ? "Ja" : "Nee") . '</p>';
                 echo '<p><strong>Categorie:</strong> ' . $row["categorie"] . '</p>';
-                echo '<p><strong>Aantal voorraad:</strong> ' . $row["aantal_voorraad"] . '</p>';
                 echo '</div>';
                 echo '<img src="fotos/' . $row["afbeelding"] . '" alt="' . $row["naam"] . '">';
                 echo '</div>';
             }
         } else {
-            echo "<p>Geen recepten gevonden.</p>";
+            echo "<p class='right'>No recipes found.</p>";
         }
 
         mysqli_close($conn);
         ?>
+
     </div>
 
-    <footer>
-        <p>hello</p>
-    </footer>
+
+    <!-- <?php
+            require('footer.php');
+            ?> -->
+
 </body>
 
 </html>
